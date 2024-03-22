4d4d44570f8fd4bbf23c14e7c69edac982ae48a5
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-03-21 17:23:48 -0700

    modified the position of the Join button

--- components/EventItem.js
+++ components/EventItem.js
@@ -19,17 +19,20 @@ export default function EventItem({ name, location, time, imageUrl }) {
         <Text style={styles.overlayText}>{time}</Text>
       </View>
       <Text style={styles.eventName}>{name}</Text>
-      <View>
-        <Text style={styles.eventDetail}>Location: {location}</Text>
-        <Text style={styles.eventDetail}>Time: {time}</Text>
+      <View style={styles.container}>
+        <View style={styles.eventDetailContainer}>
+          <Text style={styles.eventDetail}>Location: {location}</Text>
+          <Text style={styles.eventDetail}>Time: {time}</Text>
+        </View>
+        <View style={styles.joinButtonContainer}>
+          <PressableButton
+            backgroundColor={colors.backgroundlight}
+            onPress={joinHandler}
+          >
+            <Text style={styles.buttonText}>Join</Text>
+          </PressableButton>
+        </View>
       </View>
-
-      <PressableButton
-        backgroundColor={colors.backgroundlight}
-        onPress={joinHandler}
-      >
-        <Text style={styles.buttonText}>Join</Text>
-      </PressableButton>
     </View>
   );
 }
@@ -55,6 +58,12 @@ const styles = StyleSheet.create({
   overlayText: {
     color: "#ffffff",
   },
+  container: {
+    flexDirection: "row",
+  },
+  eventDetailContainer: {
+    flex: 3,
+  },
   eventName: {
     fontSize: 30,
     color: "#ffffff",
@@ -64,6 +73,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#ffffff",
     paddingBottom: 5,
+  },
+  joinButtonContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
   },
   buttonText: {
     color: "#ffffff",
